02f2d257b94478b05476757e1bcca950afa5e491
ShigrafS <shigrafsalik@proton.me>
2026-04-03 23:17:04 +0530

    build(gradle): add ktlint plugin for code style enforcement
    Configure the org.jlleitschuh.gradle.ktlint plugin within the root
    build script and apply it across all subprojects. This enables the
    ktlintCheck task required by the CI pipeline to enforce consistent
    Kotlin formatting throughout the repository.
    
    Signed-off-by: ShigrafS <shigrafsalik@proton.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,9 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.5"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
